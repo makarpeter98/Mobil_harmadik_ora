f118cf2f5c626ab77707b7100ab5abc76d62b4e1
makarpeter <makar.peter98@gmail.com>
2025-03-04 14:36:43 +0100

    MainActivity felkesz

--- app/src/main/java/com/example/mobil_harmadik_ora/MainActivity.java
+++ app/src/main/java/com/example/mobil_harmadik_ora/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.mobil_harmadik_ora;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    SensorManager sensorManager;
+    TextView sensorsTextView;
+    TextView lightSensorTextView;
+    Sensor lightSensor;
+    SensorEventListener lightSensorEventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        sensorsTextView = findViewById(R.id.sensorsTextView);
+        lightSensorTextView = findViewById(R.id.lightSensorTextView);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(lightSensorEventListener, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(lightSensorEventListener);
     }
 }
